abba3807cbfc6fdb7803b87c742c7c9da5822b02
wang <wolf@wangdeMacBook-Pro.local>
2014-12-06 17:13:13 +0800

    5-1
    cpp 作业

--- sy51/main.cpp
+++ sy51/main.cpp
@@ -7,9 +7,80 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <string>
+using namespace std;
+class Fan
+{
+private:
+    double speed;
+    bool on;
+    double radius;
+    string color;
+public:
+    void getSpeed();
+    void setSpeed(double);
+    void getOn();
+    void setOn(bool);
+    void getRadius();
+    void setRadius(double);
+    void getColor();
+    void setColor(string);
+    Fan(double newSpeed, bool newOn, double newRadius, string newColor);
+    void display();
+};
+int main()
+{
+    Fan fan1(3, true, 10, "yellow");
+    Fan fan2(2, true, 5, "blue");
+    fan1.display();
+    fan2.display();
+    
     return 0;
+}
+
+Fan::Fan(double newSpeed, bool newOn, double newRadius, string newColor)
+{
+    this->speed = newSpeed;
+    this->on = newOn;
+    this->radius = newRadius;
+    this->color = newColor;
+}
+void Fan::getSpeed()
+{
+    cout << "Speed : " << this->speed << endl;
+}
+void Fan::setSpeed(double newSpeed)
+{
+    this->speed = newSpeed;
+}
+void Fan::getOn()
+{
+    cout << "On : " << this->on << endl;
+}
+void Fan::setOn(bool newOn)
+{
+    this->on = newOn;
+}
+void Fan::getRadius()
+{
+    cout << "Radius : " << this->radius << endl;
+}
+void Fan::setRadius(double newRadius)
+{
+    this->radius = newRadius;
+}
+void Fan::getColor()
+{
+    cout << "Color : " << this->color << endl;
+}
+void Fan::setColor(string newColor)
+{
+    this->color = newColor;
+}
+void Fan::display()
+{
+    this->getSpeed();
+    this->getOn();
+    this->getRadius();
+    this->getColor();
 }
